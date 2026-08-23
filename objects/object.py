02dcb1080419f69a8e19cc514259f0ccdaa870bc
Ph4ntom-ops <ph4ntom.eclip5e@gmail.com>
2026-08-23 20:43:37 +0545

    updated object.py with some new print statements and methods

--- objects/object.py
+++ objects/object.py
@@ -1,12 +1,22 @@
-class Car:
-    def __init__(self, model, year, color, for_sale):
-        self.model = model
-        self.year = year
-        self.color = color
-        self.for_sale = for_sale
+from car import Car
 
 car1 = Car("Toyota Supra", 2017, "Black", False)
 car2 = Car("Lamborghini", 2018, "Green", True)
 car3 = Car("Porche", 2006, "White", False)
 
-print(car3.model)
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
+
+print()
+
+car1.drive()
+car2.drive()
+car3.drive()
+
+print()
+
+car1.describe()
+car2.describe()
+car3.describe()
